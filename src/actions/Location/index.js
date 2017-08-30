92d630f079412266f30ccb76e4108cd443bcbacb
Hima <hima_chhag@yahoo.com>
2017-08-30 00:57:23 -0400

    Making changes to solve dispatch isuues

--- src/actions/Location/index.js
+++ src/actions/Location/index.js
@@ -1,37 +1,39 @@
-// import React from 'react'
-// import { Field, reduxForm, SubmissionError } from 'redux-form';
-
-// import { stopFetchingData, startFetchingData } from '../fetchingData';
 import { API_URL } from '../../global';
 
 export function submitToServer(url, data) {
 
   console.log(JSON.stringify({"location": data}))
-    return dispatch => {
-      fetch(API_URL + url, {
-      method:'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({"location": data})
-    }).then(function(response) {
-          if (response.status >= 400) {
-              console.log("Error " + response.status + " in the AJAX call")
-              throw new Error("Bad response from server");
-          }
-          const responseJson = response.json()
-          console.log(responseJson)
-          dispatch({
-              type  : 'GET_LOCATION_DETAILS',
-              payload : response.json()
-          });
+      debugger;
+      const request = fetch(API_URL + url, {
+        method:'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({"location": data})
       })
-      .catch(function(error) {
-          dispatch({
-              type  : 'ERROR_LOCATION',
-              payload : error
+
+      return (dispatch) => {
+        console.log('HI')
+        return request.then(function(response) {
+          debugger;
+              if (response.status >= 400) {
+                  console.log("Error " + response.status + " in the AJAX call")
+                  throw new Error("Bad response from server");
+              }
+              const responseJson = response.json()
+              console.log(responseJson)
+              dispatch({
+                  type  : 'GET_LOCATION_DETAILS',
+                  payload : response.json()
+              });
+          })
+          .catch(function(error) {
+            debugger;
+              dispatch({
+                  type  : 'ERROR_LOCATION',
+                  payload : error
+              });
           });
-      });
     }
 }
--- src/actions/fetchingData.js
+++
@@ -1,7 +0,0 @@
-export const stopFetchingData = (dataType) => {
-  return { type: `STOP_FETCHING_${dataType}`, dataType }
-}
-
-export const startFetchingData = (dataType) => {
-  return { type: `START_FETCHING_${dataType}`, dataType }
-}
