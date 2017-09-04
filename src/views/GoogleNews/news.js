4ee47cb790b65be4ff18f16ef7db097534e65989
Hima <hima_chhag@yahoo.com>
2017-09-04 14:55:04 -0400

    changing API_URL for heroku

---
+++ src/views/GoogleNews/news.js
@@ -0,0 +1,7 @@
+// const bingNews = require("bingnews")("API_KEY")
+//  
+// // bingNews({ "query": "rabbits", "category": "politics" }, function(error, articles) {
+// //   console.log(articles)
+// // })
+// 
+// export default bingNews;
