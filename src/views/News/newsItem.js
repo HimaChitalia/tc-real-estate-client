f464f989ef8b7a3794fa1cf0613581333327cd5d
Hima <hima_chhag@yahoo.com>
2017-09-08 22:17:09 -0400

    Added thumbs up, thumbs down icon instead of buttons

--- src/views/News/newsItem.js
+++ src/views/News/newsItem.js
@@ -2,6 +2,10 @@ import React, { Component} from 'react';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { updateArticleLikesAndDislikes } from '../../actions/updateNews';
+
+import upImg from '../../images/Thumbs-Up-48-to.png';
+import downImg from '../../images/Thumbs-Down-48-to.png';
+
 import '../../styles/custom.css'
 
 class NewsItem extends Component {
@@ -56,9 +60,10 @@ class NewsItem extends Component {
     return(
       <div key={item.key} >
         <h4> - <a href={item.url} target="_blank" rel="noopener noreferrer">{item.title} </a></h4>
-        <button onClick ={ this.changeCount }  data-likes={item.likes} value={item.key}>Upvote { this.state.likes }</button>
-        <p className='inlineText'>{ this.state.likes - this.state.dislikes}</p>
-        <button onClick ={ this.changeCount }  data-dislikes={item.dislikes} value={item.key}>Downvote { this.state.dislikes }</button>
+        <input type="image" src={upImg} onClick ={ this.changeCount }  data-likes={item.likes} value={item.key} alt="Upvote"/>
+        <p className='inlineText'>{ this.state.likes }</p>
+        <input type="image" src={downImg} onClick ={ this.changeCount }  data-dislikes={item.dislikes} value={item.key} alt="Downvote"/>
+        <p className='inlineText'>{ this.state.dislikes }</p>
       </div>
     )
   }
@@ -71,3 +76,9 @@ const mapDispatchToProps = dispatch => {
 
 
 export default connect(null, mapDispatchToProps)(NewsItem);
+
+
+
+        // <button onClick ={ this.changeCount }  data-likes={item.likes} value={item.key}>Upvote { this.state.likes }</button>
+        // <p className='inlineText'></p>
+        // <button onClick ={ this.changeCount }  data-dislikes={item.dislikes} value={item.key}>Downvote { this.state.dislikes }</button>
